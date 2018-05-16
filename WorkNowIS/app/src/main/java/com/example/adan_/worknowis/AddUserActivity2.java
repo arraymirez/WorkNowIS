f41708e6e2e5c2b6f1d2d53d35962e731a0ebf40
Jennifer Llamas <jennifer.llamas@tectijuana.edu.mx>
2018-05-15 22:38:14 -0700

    Login y creacion de usuarios, hay algunos metodos, y la ventana de citas mañana checamos estructura

---
+++ WorkNowIS/app/src/main/java/com/example/adan_/worknowis/AddUserActivity2.java
@@ -0,0 +1,13 @@
+package com.example.adan_.worknowis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddUserActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_user2);
+    }
+}
