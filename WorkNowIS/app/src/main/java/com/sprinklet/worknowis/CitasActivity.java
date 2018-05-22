f58c6384b49468a888d6e08003972c9695d73888
Adan Ramirez Grande <adan.ramirez17@tectijuana.edu.mx>
2018-05-22 11:49:14 -0800

    marcadores creados y enlace entre mapas y login. Boton de crear vista de citas fallando

--- WorkNowIS/app/src/main/java/com/sprinklet/worknowis/CitasActivity.java
+++ WorkNowIS/app/src/main/java/com/sprinklet/worknowis/CitasActivity.java
@@ -5,27 +5,36 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.sprinklet.worknowis.R;
 
 public class CitasActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_citas);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ObtenerInfo();
+    }
+
+
+
+
+
+
+    public void ObtenerInfo(){
+        String id=getIntent().getStringExtra("idEmpleado");
+        EditText inputTitulo= findViewById(R.id.txtNombreActividad);
+        EditText inputCosto= findViewById(R.id.txtCosto);
+        inputTitulo.setText("Plomeeria", TextView.BufferType.NORMAL);
+        inputCosto.setText("$350", TextView.BufferType.NORMAL);
+        /*Aqui deberia poner el metodo o la busqueda de info del empleddo*/
     }
 
 }
--- WorkNowIS/app/src/main/java/com/sprinklet/worknowis/MapsActivity.java
+++ WorkNowIS/app/src/main/java/com/sprinklet/worknowis/MapsActivity.java
@@ -1,47 +1,97 @@
 package com.sprinklet.worknowis;
-
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.location.Location;
-
+import android.util.Log;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.sprinklet.classes.MySQLConnection;
+import com.sprinklet.worknowis.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.sprinklet.worknowis.R;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import static android.widget.Toast.*;
+
+public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarkerClickListener,OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    Marker seleccionado;
+    LocationManager locationManager;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Button crearCita=(Button)findViewById(R.id.btnCita);
+        crearCita.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if(seleccionado!=null){
+
+                    Intent aCita=new Intent(MapsActivity.this,CitasActivity.class);
+                    aCita.putExtra("idEmpleado",seleccionado.getTag().toString());
+                    startActivity(aCita);
+                }
+
+            }
+
+
+        });
+
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        Location loc;
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setMyLocationEnabled(true);
+
+        // Se deben generar los puntos de los empleados
+        Marcadores(googleMap);
+
+    }
+ public void Marcadores(GoogleMap googleMap){
+        mMap =googleMap;
+        int cantidad=1;//estos deberian ser la consulta de la bd
+        for (int a=0; a<cantidad;a++){
+            final LatLng p=new LatLng(32.52,-116.88);//aqui deben de ir las coordenadas de la bd
+            Marker punto= mMap.addMarker(new MarkerOptions().position(p).title("Nombre de empleado"));
+
+         }
+     mMap.setOnMarkerClickListener(this);
+ }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        if(marker.isInfoWindowShown()){
+            marker.hideInfoWindow();
+            seleccionado=null;
+        }else {
+
+            marker.showInfoWindow();
+            seleccionado=marker;
+        }
+
+        return true;
     }
 }
